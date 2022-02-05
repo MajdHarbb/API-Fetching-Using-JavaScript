20a0b5b21aef9b28e9cd202756d07d59b1daca76
MajdHarbb <majdwalidharb@gmail.com>
2022-02-05 17:25:47 +0200

    display data

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
-/* Fetch data using async await */
+/* JS code to fetch data using async await */
+let search = document.getElementById("search-btn");
+let name = document.getElementById("name-span");
+let gender = document.getElementById("gender-span");
+let age = document.getElementById("age-span");
+let country = document.getElementById("nationality-span");
 
 /* fetch random dog image */
 async function fetchDogImage() {
@@ -6,33 +11,53 @@ async function fetchDogImage() {
   const dogs = await response.json();
   return dogs;
 }
+
 /* Display dog image */
 fetchDogImage().then((dogs) => {
   dogs;
   console.log(dogs);
   document.getElementById("dog-image").src = dogs.message;
 });
-let search = document.getElementById("search-btn");
+
+/* fetch data on button click */
 search.onclick = function () {
   
-  let name = document.getElementById("name-input");
-  test = name.value;
-  const fetchData = async () => {
-    try {
-      const [genderize, agify, nationalize] = await Promise.all([
-        fetch(`https://api.genderize.io?name=${test}`),
-        fetch(`https://api.agify.io/?name=${test}`),
-        fetch(`https://api.nationalize.io/?name=${test}`),
-      ]);
-      const todoOne = await genderize.json();
-      const todoTwo = await agify.json();
-      const todoThree = await nationalize.json();
-      console.log(todoOne.gender);
-      console.log(todoTwo.age);
-      console.log(todoThree.country);
-    } catch (err) {
-      throw err;
-    }
-  };
-  fetchData();
+  let input = document.getElementById("name-input");
+  country.innerText = "";
+  /* If input field is null --> alert the user*/
+  if(input.value == "" ){
+      alert("Please Insert A Name First!");
+      
+  } else{
+    const fetchData = async () => {
+        try {
+          const [genderize, agify, nationalize] = await Promise.all([
+            fetch(`https://api.genderize.io?name=${input.value}`),
+            fetch(`https://api.agify.io/?name=${input.value}`),
+            fetch(`https://api.nationalize.io/?name=${input.value}`),
+          ]);
+    
+          const returned_gender = await genderize.json();
+          const returned_age = await agify.json();
+          const returned_nationality = await nationalize.json();
+
+          name.innerText = input.value;
+          gender.innerText = returned_gender.gender;
+          age.innerText = returned_age.age;
+          console.log(returned_gender.gender);
+          console.log(returned_age.age);
+
+          for(var i=0; i<returned_nationality.country.length; i++){
+            console.log(returned_nationality.country[i].country_id);
+            country.innerText += " "+returned_nationality.country[i].country_id+" ";
+        }
+          //console.log(returned_nationality.country.country_id);
+    
+        } catch (err) {
+          throw err;
+        }
+      };
+      fetchData();
+  }
+ 
 };
