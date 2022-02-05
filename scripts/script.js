e78db8da1c399ff697e69b4a2b85903c95c1f827
MajdHarbb <majdwalidharb@gmail.com>
2022-02-05 15:27:41 +0200

    fetch all data from APIs

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,38 @@
-async function fetchMoviesJSON() {
-    const response = await fetch('https://dog.ceo/api/breeds/image/random');
-    const movies = await response.json();
-    return movies;
-  }
-  fetchMoviesJSON().then(movies => {
-    movies; // fetched movies
-    console.log(movies);
-    document.getElementById("dog-image").src=movies.message;
-  });
+/* Fetch data using async await */
 
-  fetch('https://api.genderize.io?name=caline')
-  .then(response => response.json())
-  .then(data => console.log(data));
+/* fetch random dog image */
+async function fetchDogImage() {
+  const response = await fetch("https://dog.ceo/api/breeds/image/random");
+  const dogs = await response.json();
+  return dogs;
+}
+/* Display dog image */
+fetchDogImage().then((dogs) => {
+  dogs;
+  console.log(dogs);
+  document.getElementById("dog-image").src = dogs.message;
+});
+let search = document.getElementById("search-btn");
+search.onclick = function () {
+  
+  let name = document.getElementById("name-input");
+  test = name.value;
+  const fetchData = async () => {
+    try {
+      const [genderize, agify, nationalize] = await Promise.all([
+        fetch(`https://api.genderize.io?name=${test}`),
+        fetch(`https://api.agify.io/?name=${test}`),
+        fetch(`https://api.nationalize.io/?name=${test}`),
+      ]);
+      const todoOne = await genderize.json();
+      const todoTwo = await agify.json();
+      const todoThree = await nationalize.json();
+      console.log(todoOne.gender);
+      console.log(todoTwo.age);
+      console.log(todoThree.country);
+    } catch (err) {
+      throw err;
+    }
+  };
+  fetchData();
+};
